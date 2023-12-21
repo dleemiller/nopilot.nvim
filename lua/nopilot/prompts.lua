34a1d8021b08988c9ef5bca42f3d897b60dbe2a3
Lee Miller <dlmiller@adventhp.com>
2023-12-20 21:15:44 -0700

    add descriptions, update prompts, starting system prompt

--- lua/nopilot/prompts.lua
+++ lua/nopilot/prompts.lua
@@ -1,60 +1,34 @@
 return {
-    chat = { prompt = "$user" },
-    annotate = {
-        prompt = "Add type annotations, but do not change the code or indentation.\n\nHere is the code:\n$visual\n\nRemember to respond only with code inside a code block.",
-        replace = false,
+    alter = {
+      prompt = "Here is a section of code that needs to be rewritten:\n$visual\n\nUse these instructions to make your alterations:\n$user\n\nRemember to only respond with the new code inside a code block. Do not supply test code.",
+      description = "alter the selection",
+      replace = false,
     },
-
-    docstring = {
-        prompt = "Add docstrings, but do not change the code or indentation.\n\nHere is the code:\n$visual\n\nRemember to respond only with code inside a code block.",
+    chat = {
+        prompt = "$user",
         replace = false,
+        description = "user message only",
+        system = "You are a helpful expert programmer and teacher.",
+    },
+    complete = {
+      prompt = "Complete the function, given the function definition and docstring.\n\nHere is the function to complete:\n$visual\n\nRemember to only respond with the new code inside a code block.",
+      description="complete the function from docstring and def",
+      replace = false,
+    },
+    debug = {
+        prompt = "Identify bugs in the following code:\n\n$visual",
+        replace = false,
+        description = "find bugs in selection",
     },
     document = {
         prompt = "Add a docstrings and type annotations, but do not change the code or indentation.\n\nHere is the code:\n$visual\n\nRemember to only respond with code inside a code block.",
         replace = false,
+        description = "apply documentation to selection",
     },
-
-    func = {
-        prompt = "Generate a python function with a docstring.\n\nHere is the function description:\n$user\n\nRemember to only respond with code inside a code block. Do not supply test code.",
-        replace = false,
-    },
-
-    context = {
-        prompt = "Generate a python function with a docstring.\n\nHere is the context from the script to use as a reference:\n$visual\n\nHere is the function description:\n$user\n\nRemember to only respond with code inside a code block. Do not supply test code. Be sure to write the new function in context with the provided script.",
-        replace = false,
-    },
-
-    alter = {
-      prompt = "Here is a section of code that needs to be rewritten:\n$visual\n\nUse these instructions to make your alterations:\n$user\n\nRemember to only respond with the new code inside a code block. Do not supply test code.",
-      replace = false,
-    },
-
-    autotests = {
-      prompt = "Here is a section of code that needs to have tests written:\n$visual\n\nMock interfaces and objects as needed.\n\nRemember to only respond with the new code inside a code block.",
-      replace = false,
-    },
-
-    vars = {
-      prompt = "Use best practices to improve the naming of variables, functions or class names in this script. Only change the names of local variables.\n\nHere is the code:\n$visual\n\nRemember to only respond with the new code inside a code block. It is not safe to change the names of functions, arguments or things that are not local.",
-      replace = false,
-    },
-
-    complete = {
-      prompt = "Complete the function, given the function definition and docstring.\n\nHere is the function to complete:\n$visual\n\nRemember to only respond with the new code inside a code block.",
-      replace = false,
-    },
-
     query = {
       prompt = "Use the provided code as context, along with expert knowledge to answer the user's question.\n\nHere is the code:\n$visual\n\nQuestion:\n$user",
       replace = false,
+      description = "ask a question about selection",
     },
-
-    plan = {
-      prompt = "You will assist a user in planning a project. Your job is to do the planning and present it to the user.\n\nIf the user has started a plan it is here:\n$visual\n\nUser's request:\n$user",
-      replace = false,
-      options = {
-          temperature = 1.2
-      }
-    }
 }
 
